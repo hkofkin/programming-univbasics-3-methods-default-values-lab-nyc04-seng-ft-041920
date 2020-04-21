761d1a3f6b2c9c0821a86f282be8a2fe7dba29c5
Hannah Kofkin <hkofkin@gmail.com>
2020-04-21 22:19:20 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,7 +3,7 @@
 
 def meal_choice(veg1, side, protein="tofu")
   puts "What a nutritious meal!"
-  "A plate of #{protein} with #{veg1} and #{side}."
+  puts "A plate of #{protein} with #{veg1} and #{side}."
 end
 
 meal_choice("broccoli", "macaroni", "roast beef")
